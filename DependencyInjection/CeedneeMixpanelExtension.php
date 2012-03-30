eba2bc1c4285a8a44d50ac1985b07426b42aabd6
sidney curron <ceednee@gmail.com>
2012-03-30 18:29:53 +0200

    Added more config checked

--- DependencyInjection/CeedneeMixpanelExtension.php
+++ DependencyInjection/CeedneeMixpanelExtension.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
- * This is the class that loads and manages your bundle configuration
+ * This is the class that loads and manages the bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
@@ -25,14 +25,22 @@ class CeedneeMixpanelExtension extends Extension
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
 
-        if (!isset($config['api_key'])) {
+        if (! isset($config['api_key'])) {
             throw new \InvalidArgumentException('The "api_key" option must be set');
         }
-        $container->setParameter('ceednee_mixpanel.api_key', $config['api_key']);
+        $container->setParameter('ceednee.mixpanel.api_key', $config['api_key']);
 
-        if (!isset($config['api_secret'])) {
+        if (! isset($config['api_secret'])) {
             throw new \InvalidArgumentException('The "api_secret" option must be set');
         }
-        $container->setParameter('ceednee_mixpanel.api_secret', $config['api_secret']);
+        $container->setParameter('ceednee.mixpanel.api_secret', $config['api_secret']);
+
+        if (! empty($config['expire'])) {
+            $container->setParameter('ceednee.mixpanel.expire', $config['expire']);
+        }
+
+        if (! empty($config['api_url'])) {
+            $container->setParameter('ceednee.mixpanel.api_url', $config['api_url']);
+        }
     }
 }
