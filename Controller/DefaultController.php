27d9d2af3ca63944c7115ea6293576bd900efc75
sidney curron <ceednee@gmail.com>
2012-04-03 17:44:08 +0200

    Tests

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -11,11 +11,21 @@ class DefaultController extends Controller
     /**
      * @Route("/hello/{name}")
      * @Template()
+     * @param $name
      */
     public function indexAction($name)
     {
         $event = $this->get('ceednee.mixpanel.event');
-        print $event->generateSignature();
+        //var_dump($event->calculateSignature());
+
+        var_dump($event->getEvents(
+                array(
+                    'event' => array('play song', 'log in'),
+                    'type' => 'general',
+                    'unit' => 'hours',
+                    'interval' => 2)
+            )
+        );
 
         //return array('name' => $name);
     }
